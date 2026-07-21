b0119637c79ae6998245eb82360d71902fc28117
saikambala06 <saikambala777@gmail.com>
2026-07-21 12:48:02 +0530

    Delete src directory

--- src/lib/tokens.ts
+++
@@ -1,8 +0,0 @@
-import { customAlphabet } from "nanoid";
-
-const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-const gen = customAlphabet(alphabet, 40);
-
-export function generateApiToken() {
-  return `jaf_${gen()}`;
-}
